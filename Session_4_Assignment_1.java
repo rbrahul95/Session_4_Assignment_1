3ee5fb6fca3f663d18ed96f6c7ad1c4c690c6a25
rbrahul95 <rbrahul95@gmail.com>
2016-08-28 12:44:20 +0530

    Add files via upload

---
+++ Session_4_Assignment_1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package acadglid;
+
+/**
+ *
+ * @author My
+ */
+public class Session_4_Assignment_1 {
+    public static void main (String[] args){ 
+int arrnum [] = new int []{12,10,100,67,90}; 
+// print the original array 
+System.out.println("printing original array"); 
+for (int i=0;i<arrnum.length;i++){ 
+System.out.println(arrnum[i]); 
+} 
+// print in reverse 
+System.out.println("printing reversed array"); 
+for (int i = arrnum.length - 1;i>=0;i--){ 
+System.out.println(arrnum[i]); 
+} 
+}
+}
